8078bb8413122eef6c154e4a7e3cd8af9e4b507c
Michał Czmiel <michalczmiel@outlook.com>
2022-03-24 15:07:24 +0100

    refactor: use hook for accessig data

---
+++ src/hooks/useRadarData.ts
@@ -0,0 +1,57 @@
+import { graphql, useStaticQuery } from "gatsby";
+import { Entry, Quadrant, Ring } from "../models/radar";
+
+interface LocalDataQueryData {
+  allEntriesJson: {
+    nodes: Entry[];
+  };
+  allQuadrantsJson: {
+    nodes: Quadrant[];
+  };
+  allRingsJson: {
+    nodes: Ring[];
+  };
+}
+
+export const LocalDataQuery = graphql`
+  query {
+    allRingsJson {
+      nodes {
+        name
+        color
+      }
+    }
+
+    allQuadrantsJson {
+      nodes {
+        name
+      }
+    }
+
+    allEntriesJson {
+      nodes {
+        ring
+        quadrant
+        label
+        moved
+        active
+      }
+    }
+  }
+`;
+
+interface UseRadarData {
+  rings: Ring[];
+  quadrants: Quadrant[];
+  entries: Entry[];
+}
+
+export function useRadarData(): UseRadarData {
+  const data = useStaticQuery<LocalDataQueryData>(LocalDataQuery);
+
+  const entries = data.allEntriesJson.nodes;
+  const rings = data.allRingsJson.nodes;
+  const quadrants = data.allQuadrantsJson.nodes;
+
+  return { entries, rings, quadrants };
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,10 @@
 import * as React from "react";
-import { graphql } from "gatsby";
 import { Radar } from "../components/Radar";
-import { Entry, Quadrant, Ring } from "../models/radar";
+import { useRadarData } from "../hooks/useRadarData";
 import "./index.css";
 
-export const pageQuery = graphql`
-  query {
-    allRingsJson {
-      nodes {
-        name
-        color
-      }
-    }
-
-    allQuadrantsJson {
-      nodes {
-        name
-      }
-    }
-
-    allEntriesJson {
-      nodes {
-        ring
-        quadrant
-        label
-        moved
-        active
-      }
-    }
-  }
-`;
-
-interface Props {
-  data: {
-    allEntriesJson: {
-      nodes: Entry[];
-    };
-    allQuadrantsJson: {
-      nodes: Quadrant[];
-    };
-    allRingsJson: {
-      nodes: Ring[];
-    };
-  };
-}
-
-const IndexPage = (props: Props) => {
-  const entries = props.data.allEntriesJson.nodes;
-  const rings = props.data.allRingsJson.nodes;
-  const quadrants = props.data.allQuadrantsJson.nodes;
+const IndexPage = () => {
+  const { quadrants, entries, rings } = useRadarData();
 
   return (
     <main>
