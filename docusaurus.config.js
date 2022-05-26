25927ca6489c2598649708112daa42659eed7af9
Michał Czmiel <michalczmiel@outlook.com>
2022-05-26 10:29:34 +0200

    feat: update GitHub link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/boldare/boldare-radar",
             label: "GitHub",
             position: "right",
           },
