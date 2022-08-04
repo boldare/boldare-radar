e1db7c51587b6d21bfa122057d48f8143a1244df
katarzynawietecha <katarzyna.wietecha@boldare.com>
2022-08-04 14:50:52 +0200

    feat: change of overwritten class name

--- src/components/Landing/Menu/Menu.tsx
+++ src/components/Landing/Menu/Menu.tsx
@@ -4,21 +4,21 @@ import "./Menu.css";
 
 export default function Menu() {
   return (
-    <div className={"menu"}>
-      <div className={"container menu__container"}>
+    <div className={"menu-landing"}>
+      <div className={"container menu-landing__container"}>
         <img
           src={"img/logo-boldare.svg"}
           alt={"Boldare logo"}
-          className={"menu__logo"}
+          className={"menu-landing__logo"}
         />
-        <nav className={"menu__nav"}>
-          <a href={"#into"} className={"menu__link"}>
+        <nav className={"menu-landing__nav"}>
+          <a href={"#into"} className={"menu-landing__link"}>
             What?
           </a>
-          <a href={"#instructions"} className={"menu__link"}>
+          <a href={"#instructions"} className={"menu-landing__link"}>
             Instructions
           </a>
-          <a href={"#tech-radar"} className={"menu__link"}>
+          <a href={"#tech-radar"} className={"menu-landing__link"}>
             Tech&nbsp;Radar
           </a>
         </nav>
