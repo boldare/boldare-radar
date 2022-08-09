41106aa0dd52109c9113ae71a1f721c82d4dc924
aleksanderujek <aleksander.ujek@boldare.com>
2022-08-09 14:06:16 +0200

    fix: build process ignores links for categories
    during build process categories are "broken links" but after the build
    those links are available

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   tagline: "Boldare Radar",
   url: "https://boldare.com",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/icon.png",
   i18n: {
@@ -76,12 +76,6 @@ const config = {
         },
         items: [
           {
-            type: "doc",
-            docId: "/category/prototype",
-            position: "left",
-            label: "Docs",
-          },
-          {
             href: "https://github.com/boldare/boldare-radar",
             label: "GitHub",
             position: "right",
@@ -104,7 +98,7 @@ const config = {
               },
               {
                 label: "Product Market Fit",
-                to: "/docs/category/pmf",
+                to: "/docs/category/product-market-fit",
               },
               {
                 label: "Scaling",
