a86cfcefcf3b78e04ec3af3d799ce43264993d6a
Filip Puławski <filip.pulawski@gmail.com>
2022-10-06 15:41:57 +0200

    Revert "ci: deploy artifacts to tech-radar subdirectory"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "📨 Upload files" # and remove non existing files
-aws s3 sync ./build s3://$FRONTEND_BUCKET_NAME/tech-radar/ --delete --cache-control max-age=300,public
+aws s3 sync ./build s3://$FRONTEND_BUCKET_NAME --delete --cache-control max-age=300,public
 
 echo "💨 Invalidate cache"
 aws cloudfront create-invalidation --distribution-id $FRONTEND_CLOUDFRONT_ID --paths "/*"
